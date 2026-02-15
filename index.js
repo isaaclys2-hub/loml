a6cc0d9252dd444a063fbf9c6c72469fe675d1e5
Isaac <isaaclys2@gmail.com>
2026-02-15 17:15:13 +0800

    Initial commit - Valentine site

--- index.js
+++ index.js
@@ -1,98 +1,21 @@
-const box = document.querySelector('.box');
-
-function launchConfetti() {
-    let count = 0;
-    const interval = setInterval(() => {
-        confetti({
-            particleCount: 100,
-            spread: 70,
-            origin: { y: 0.6 }
-        });
-        count++;
-        if (count === 5) clearInterval(interval); // 5 bursts
-    }, 300);
+function showMessage(){
+    document.getElementById("secretMessage").style.display = "block";
 }
 
-function renderFinalLove() {
-    box.innerHTML = `
-    <img src="catto.gif" />
-    <h2>YAYYY I LOPPPP UUUU TOO! ❤️❤️🤍🤍</h2>
-    `;
-    launchConfetti()
+/* floating hearts */
+setInterval(createHeart, 350);
+
+function createHeart(){
+    const heart = document.createElement("div");
+    heart.classList.add("heart");
+    heart.innerHTML = "❤";
+
+    heart.style.left = Math.random() * 100 + "vw";
+    heart.style.fontSize = (Math.random() * 20 + 15) + "px";
+
+    document.getElementById("hearts-container").appendChild(heart);
+
+    setTimeout(()=>{
+        heart.remove();
+    },4000);
 }
-
-function renderFinalLove2() {
-    box.innerHTML = `
-    <img src="catto.gif" />
-    <h2>HAHAHA I KNEW ITT I LOPPPP UUUU TOO! 😁😁</h2>
-    `;
-    launchConfetti()
-}
-
-function renderFine() {
-    box.innerHTML = `
-        <img src="scoobert-mad-cat.gif" />
-        <h2>FINEE 😠😠</h2>
-        <div class="btns">
-            <button id="yes6">YES</button>
-            <button id="no6" disabled=disabled>UPS</button>
-        </div>
-    `;
-    document.getElementById('yes6').addEventListener('click', renderFinalLove2);
-}
-
-function renderDesperate() {
-    box.innerHTML = `
-        <img src="cat-woof.gif" />
-        <h2>PLEASE BABYY😭😭🥺</h2>
-        <div class="btns">
-            <button id="yes5">Yes</button>
-            <button id="no5">Nah</button>
-        </div>
-    `;
-    document.getElementById('yes5').addEventListener('click', renderFinalLove);
-    document.getElementById('no5').addEventListener('click', renderFine);
-}
-
-function renderImGonnaCry() {
-    box.innerHTML = `
-        <img src="cat-crying-cat.gif" />
-        <h2>im gonna CRYY....😔😭😭</h2>
-        <div class="btns">
-            <button id="yes4">Yes</button>
-            <button id="no4">Nah</button>
-        </div>
-    `;
-    document.getElementById('yes4').addEventListener('click', renderFinalLove);
-    document.getElementById('no4').addEventListener('click', renderDesperate);
-}
-
-function renderPlease() {
-    box.innerHTML = `
-        <img src="please-pretty-please.gif" />
-        <h2>please...??🥺😔</h2>
-        <div class="btns">
-            <button id="yes3">Yes</button>
-            <button id="no3">Nah</button>
-        </div>
-    `;
-    document.getElementById('yes3').addEventListener('click', renderFinalLove);
-    document.getElementById('no3').addEventListener('click', renderImGonnaCry);
-}
-
-function renderAreYouSure() {
-    box.innerHTML = `
-        <img src="peach-cat.gif" />
-        <h2>Are you sureee? 🥺</h2>
-        <div class="btns">
-            <button id="yes2">Yes</button>
-            <button id="no2">Nah</button>
-        </div>
-    `;
-    document.getElementById('yes2').addEventListener('click', renderFinalLove);
-    document.getElementById('no2').addEventListener('click', renderPlease);
-}
-
-
-document.getElementById('yes').addEventListener('click', renderFinalLove);
-document.getElementById('no').addEventListener('click', renderAreYouSure);
